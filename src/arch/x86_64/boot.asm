54e5b399bed9430e9ed2515924fb80ebb59bbf2c
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-05-08 10:56:15 -0500

    Replace OK with system time

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,6 +3,15 @@ global start
 section .text
 bits 32
 start:
-    ; print `OK` to screen
-    mov dword [0xb8000], 0x2f4b2f4f
-    hlt
+    ; print Time to screen
+    MOV   DL, 0Ah ; divide by: 10
+    DIV   DL      ; first digit in AL (quotient), second digit in AH (remainder)
+    MOV   DX, AX  ; save the digits
+    ADD   AL, 30h ; ASCII '0'
+    MOV   AH, 0Eh ; set up print
+    INT   10h     ; print first digit.
+    MOV   AL, DH  ; retrieve second digit
+    ADD   AL, 30h
+    INT   10h     ; print it
+    RET
+    loop start
