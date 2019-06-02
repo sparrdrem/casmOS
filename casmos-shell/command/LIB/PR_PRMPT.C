f236a11e338718d5cd106eb6a499a4ba8be1e77e
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-06-01 18:05:41 -0700

    Add casmOS Shell Source

---
+++ casmos-shell/command/LIB/PR_PRMPT.C
@@ -0,0 +1,20 @@
+/* $Id: pr_prmpt.c 528 2003-03-11 21:02:03Z skaus $
+ * print the command-line prompt
+ *
+ */
+
+#include "../config.h"
+
+
+#include "../include/command.h"
+#include "../include/misc.h"
+
+void printprompt(void)
+{	char *pr;
+
+	dbg_printmem();
+
+	pr = getEnv(PROMPTVAR);        /* get PROMPT environment var. */
+
+	displayPrompt(pr? pr: DEFAULT_PROMPT);
+}
