1a9918ed595cf337f3c5a2d802f7fe53b451bfc4
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-07-03 16:46:42 -0700

    Add the ANSI driver

---
+++ casmos-shell/ansi/makefile.bat
@@ -0,0 +1,23 @@
+@echo off
+:Script modified by SparrOSDeveloperTeam
+rem Batch file version based on the ANSI makefile.
+rem Useful if you have no MAKE tool. Otherwise, better use that.
+rem Using the Arrowsoft Assembler here - Freeware and MASM compatible.
+rem Using the Turbo C 2.x linker TLINK as LINK,
+rem because VAL does not accept files without stack segment.
+echo Replace ASM by MASM and VAL by LINK if you want.
+echo If you have MAKE, use that, not this batch script.
+echo.
+echo ANSI: ansi.asm
+asm ansi;
+echo ANSI_P: ansi_p.asm
+asm ansi_p;
+echo ANSI_F: ansi_f.asm
+asm ansi_f;
+echo ANSI_I: ansi_i.asm
+asm ansi_i;
+echo ansi.sys:	ansi.obj ansi_p.obj ansi_f.obj ansi_i.obj
+tlink /m ansi ansi_p ansi_f ansi_i;
+exe2bin ansi ansi.sys
+del ansi.exe
+del *.obj
