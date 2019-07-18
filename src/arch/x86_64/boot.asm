b7b0a1b78cb5708b3a7828d43dc3e7e75c83a144
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-07-17 22:15:16 -0500

    Fix 8 errors

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,7 +3,7 @@
 ; ====================================
 ; Changelog:
 ;
-; July 17 2019: Remove ORG and SECTION
+; July 17 2019: Make it restart
 ;
 ; November 16 2016: Create the script?
 
@@ -16,19 +16,7 @@ start:
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     int 21h
-    mov ax,07c0
-    mov ds,ax
-    xor bx,bx
-    mov ah,0e
-    mov si,offset(string)
-@1  lodsb
-    cmp al,00
-    je @2
-    int 10
-    jmps @1
-@2  xor ah,ah
-    int 16
-    callf ffff:0000
-
-string  db      '' 0a 0d 
-        db      'casmOS Alpha v0.0.2. Press any key to restart system . . .' 0a 0d 00
+    mov al, 0xFE
+    out 0x64, al
+    hlt
+    ret
